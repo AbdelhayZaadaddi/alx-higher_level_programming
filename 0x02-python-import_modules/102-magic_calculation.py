d1aba6a4eca3f467c1e7bd7056ff878a00ed58b9
abdelhay <abdelhay@gmail.com>
2023-09-07 11:19:30 -0700

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+def magic_calculation(a, b):
+    if a < b:
+        from magic_calculation_102 import add, sub
+
+        for i in range(4, 6):
+            c = add(c, i)
+    
+    else:
+        from magic_calculation_102 import sub
+    
+    return c
