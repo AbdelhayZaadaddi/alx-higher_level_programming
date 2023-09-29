80b18c01ac997e24bd34de82fe3b2ff79d2bee9e
abdelhay <abdelhay@gmail.com>
2023-09-29 02:10:57 -0700

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+The add_integer module
+
+This module supplies one function, add_integer()
+"""
+
+
+def add_integer(a, b=98):
+    """
+    """
+    if type(a) in [int, float]:
+        try:
+            a = int(a)
+        except:
+            raise TypeError("a must be an integer")
+    else:
+        raise TypeError("a must be an integer")
+    
+    if type(b) in [int, float]:
+        try:
+            b = int(b)
+        except:
+            raise TypeError("b must be an integer")
+    else:
+        raise TypeError("b must be an integer")
+    
+    return a + b
