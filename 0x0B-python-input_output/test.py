3750ab93ea489b90b5ccbfe7d59b560b1cc14f89
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-10-10 12:50:10 +0100

    task 07

--- 0x0B-python-input_output/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""read_file module.
-
-Containes a function that reads a text file.
-"""
-
-def append_write(filename="", text=""):
-    with open(filename, 'a') as f:
-        return f.write(text)
