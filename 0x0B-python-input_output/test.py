0d5493c5550a59f5da0227197ef29ddcbc82bb1a
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-10-10 13:14:39 +0100

    task 11

--- 0x0B-python-input_output/test.py
+++ 0x0B-python-input_output/test.py
@@ -6,5 +6,19 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-def to_json(self):
-    return self.__dict__
+    def to_json(self, attrs = None):
+        if attrs is not None:
+            return {k: v for k, v in slef.__dict__.items() if k  in attrs}
+        return self.__dict__
+    
+
+
+class Student():
+    def __int__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+    return self.__dict___
