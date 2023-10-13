718bda70d2f74f193d47363f41b4d0e373aaba7e
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-10-13 14:15:13 +0100

    working in task 07

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,8 +60,10 @@ class Rectangle(Base):
         return self.__width * self.__height
     
     def display(self):
-        for _ in range(self.__width):
-            print("#" * self.__width)
+        """Print the Rectangle instance with the character '#' considering x and y."""
+        print("\n" * self.__y, end="")
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
     
     def __str__(self):
         return f"[Rectangle] ({self.id} {self.__x}/{self.__y} - {self.__width}/{self.__height})"
