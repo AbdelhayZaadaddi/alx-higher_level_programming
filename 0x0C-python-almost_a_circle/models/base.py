1407a2fde356e7eb56f19dcd094a80383e9a4ac8
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-10-15 09:01:50 +0100

    done ?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,34 @@ class Base:
             else:
                 dictionaries = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation json_string."""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances from a JSON file."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                content = file.read()
+                dictionaries = cls.from_json_string(content)
+                return [cls.create(**dic) for dic in dictionaries]
+        except FileNotFoundError:
+            return []
