cc11b9f4c0a9d4ceee1bdfddc98b98d501c8651d
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-10-15 08:43:09 +0100

    mmml

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize Square instance."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return string representation of Square instance."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Getter method for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size."""
+        self.width = value
+        self.height = value
