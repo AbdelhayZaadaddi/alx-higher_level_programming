f83f629a7e4ac56afa60a96010b51c5a58dadf77
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-16 19:58:25 +0100

    wprking on task

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- -- display top 3 of cities temperature during july and august order by temperature
-SELECT city,AVR(value) AS avg_temp
+SELECT city, AVR(value) AS avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
