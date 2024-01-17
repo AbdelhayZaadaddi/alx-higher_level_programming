fa197220cdbd01e0e14b3c1f7e36e7496341202f
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-17 15:37:18 +0100

    working on task

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    satte_id INT NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (satte_id) REFRENCES states(id)
