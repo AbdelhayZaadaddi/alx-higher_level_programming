99df8baa27a39f36c639abd30f32d00504615b29
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-17 15:54:41 +0100

    working on task

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- LIST ALL SHOWS CONTAINED IN hbtn_0d_tvshows
+-- tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
