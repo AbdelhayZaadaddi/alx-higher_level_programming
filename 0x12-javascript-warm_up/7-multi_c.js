1c4853b6cb6c718396ad0b494a896d0039e24005
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-12 17:36:32 +0100

    task 07 style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@
 const num = process.argv[2];
 
 if (!num || Number.isNaN(parseInt(num, 10))) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    const x = parseInt(num, 10);
-    for (let i = 0; i < x; i++) {
-        console.log('C is fun');
-    }
+  const x = parseInt(num, 10);
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
