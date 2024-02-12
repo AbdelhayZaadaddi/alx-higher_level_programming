601968c3ac123b29ee911d06b8364f78e42656e2
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-12 18:48:32 +0100

    task 111

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2).map(Number);
-
-if (args.legth <= 1) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  args.sort((a, b) => b - a);
-  console.log(args[1]);
+  const args = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
