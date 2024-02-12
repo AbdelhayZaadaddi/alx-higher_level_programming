0f9e1c2fdd13a39e66e58b66702a33cdbf18a559
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-12 16:29:25 +0100

    task 02

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.legth > 2) {
+    for (let i = 1; i < process.argv.length; i++) {
+        console.log(process.argv[i]);
+    }
+} else {
+    console.log('No arguments');
+}
