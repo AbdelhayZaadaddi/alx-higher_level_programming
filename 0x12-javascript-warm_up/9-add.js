b80b4cba1ffbaa4120a1b953eed9f3f270fce46f
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-12 18:36:47 +0100

    task 09 tr

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
 
 function add (a, b){
   return (a + b);
