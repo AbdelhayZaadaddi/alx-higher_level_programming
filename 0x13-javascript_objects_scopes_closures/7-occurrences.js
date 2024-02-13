f5fe7e0b28677359255764e3faeae3b67524a015
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-13 08:05:43 +0100

    task 07

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let conut = 0;
+  for (let element of list) {
+    if (element === searchElement) {
+        conut++;
+    }
+  }
+  return conut;
+};
