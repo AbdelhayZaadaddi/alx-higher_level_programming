9942559bab82302f725335463f65c70cb23480ff
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-16 16:17:25 +0100

    task 02

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database, charset="utf8")
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name = '{search}'")
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(search))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
