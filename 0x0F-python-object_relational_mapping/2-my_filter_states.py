ebe0332b74c9859a6b916e5ffa2b44eb7dad78eb
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-16 18:16:44 +0100

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-
-'''
-    script that takes in an argument and displays
-    all values in the states table of
-    hbtn_0e_0_usa where name matches the argument
-'''
-
-import MySQLdb
+"""
+Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        exit(1)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    username, password = sys.argv[1], sys.argv[2]
-    database, keyword = sys.argv[3], sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                        passwd=password, db=database, charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(keyword))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
