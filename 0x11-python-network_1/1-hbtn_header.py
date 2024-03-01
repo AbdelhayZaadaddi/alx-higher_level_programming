5c6a13d6ee14880c4b6d770e5072e71d0c34cd38
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-01 15:55:04 +0100

    lpl

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,7 @@ the X-request-Id header form a given URL
 import urllib.request
 import sys
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(request) as response:
-        print(response.headers.get('W-Request-Id'))
+    reqest = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(reqest) as response:
+        print(response.headers.get('X-Request-Id'))
