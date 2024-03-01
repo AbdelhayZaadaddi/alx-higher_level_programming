cfd1142ffdf9ac3bbccec79ef3be923db2056ee5
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-01 16:00:19 +0100

    kpk

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+Script the takes in a url, send a request to the URL
+display the vody of the response
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as resoponse:
+            print(resoponse.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("Error code:", error.code)
