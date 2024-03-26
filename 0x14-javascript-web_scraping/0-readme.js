cb5ee2040f7e2937fb9cab451f2207df97abff5c
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-26 06:02:16 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+ * read the content of a file
+ * print the content of the file
+ * pint an error if the file does not exist
+*/
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log(data);
+  }
+});
